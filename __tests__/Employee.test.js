80797acf51fa1600553415299c6757083276a363
tgallagher496 <tgallagher496@gmail.com>
2022-07-18 18:15:40 -0400

    Finished libraries

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,25 @@
+const Employee = require("../lib/Employee")
+
+describe("Employee",()=>{
+    describe("test Employee's properties",()=>{
+        it ("test all properties",()=>{
+            const employee = new Employee('Tammy',"1",'tgallagher496@gmail.com')
+        
+        expect(employee.name).toEqual('Tammy')
+        expect(employee.id).toEqual("1")
+        expect(employee.email).toEqual('tgallagher496@gmail.com')
+    })
+})
+
+    describe("test Employee's methods",()=>{
+
+        it ("test all properties",()=>{
+        const employee = new Employee('Phil',"2",'ploy@yahoo.com')
+        
+        expect(employee.getName()).toEqual('Phil')
+        expect(employee.getId()).toEqual("2")
+        expect(employee.getRole()).toEqual('Employee')
+
+        })   
+    })
+})
